281e035e7781cf8833005ba468cfa3ca4f4f5b7e
Ahmed Taouaf <taouafhero@gmail.com>
2024-01-03 21:20:24 +0100

    form validators, section 07, lecture 005

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab.component.css']
 })
 export class TabComponent {
+  @Input() tabTitle=''
+  @Input() active=false
 
 }
